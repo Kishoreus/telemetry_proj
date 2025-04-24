f01643c0100f4016867d091dd4fb52831dcad734
KRITHIKus <krithikus2004@gmail.com>
2025-04-25 00:56:54 +0530

    done

--- backend/routes/order.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createorder } = require('../controllers/ordercontrollers');
-const router=express.Router();
-
-router.route('/order').post(createorder)
-
-module.exports = router;
